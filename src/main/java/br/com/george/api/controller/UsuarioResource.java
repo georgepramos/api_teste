fd68351462507d29436c033185f8d7d3430c61a3
George Paiva Ramos <georgepramos@gmail.com>
2024-03-12 20:29:29 -0300

    Controller UsuarioResource - Alteracao do findById

--- src/main/java/br/com/george/api/controller/UsuarioResource.java
+++ src/main/java/br/com/george/api/controller/UsuarioResource.java
@@ -1,6 +1,8 @@
 package br.com.george.api.controller;
 
 import br.com.george.api.domain.Usuario;
+import br.com.george.api.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
+    @Autowired
+    private UsuarioService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Usuario> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new Usuario(1,"George Ramos","georgepramos@gmail.com","654321"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
