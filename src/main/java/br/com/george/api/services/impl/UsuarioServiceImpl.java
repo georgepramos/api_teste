fa894fe6dd0916573e32bc33a2d9745a58f98b8c
George Paiva Ramos <georgepramos@gmail.com>
2024-03-12 20:27:46 -0300

    Implementacao UsuarioServiceImpl - findById

---
+++ src/main/java/br/com/george/api/services/impl/UsuarioServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.george.api.services.impl;
+
+import br.com.george.api.domain.Usuario;
+import br.com.george.api.repositories.UsuarioRepository;
+import br.com.george.api.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+    @Override
+    public Usuario findById(Integer id) {
+        Optional<Usuario> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
